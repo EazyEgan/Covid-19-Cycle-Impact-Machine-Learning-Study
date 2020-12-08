8824c72ae354dc0c2f49a19d42e53a9579a7f659
Michael Black <mblack6913@gmail.com>
2020-12-08 21:06:09 +0000

    added 2019

---
+++ cycleData2019.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("jan-oct-2020-cycle-data.csv", comment='#')
+
+GroveRoad = np.array(df.iloc[:,2])
+
+dataLen = len(GroveRoad)
+numDays = int(dataLen/24)
+
+GroveRoadByDay = np.array_split(GroveRoad, numDays)
+
+peaks = []
+#------------get peak of each day-----------------
+for i in range(0, numDays):
+    peaks.append(max(GroveRoadByDay[i]))
+
+#print(numDays, peaks)
+days = list(range(0, numDays))
+
+for i in range (0, numDays):
+    #print(i, len(x), len(GroveRoadByDay[i]))
+    plt.scatter(days[i], peaks[i])
+plt.xlabel("Hour"); plt.ylabel("Cyclists")
+plt.legend(['In'])
+plt.title("Logistic Regression")
+plt.show()
+
+hour = list(range(0,24))
+
+plt.rc('font', size=18)
+plt.rcParams['figure.constrained_layout.use'] = True
+
+for i in range (0, len(GroveRoadByDay)):
+    #print(i, len(x), len(GroveRoadByDay[i]))
+    plt.plot(hour, GroveRoadByDay[i])
+plt.xlabel("Hour"); plt.ylabel("Cyclists")
+plt.legend(['In'])
+plt.title("Logistic Regression")
+plt.show()
--- cycleData2020.py
+++ cycleData2020.py
@@ -2,27 +2,25 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("C:\college4\machineLearning\Group Project\jan-oct-2020-cycle-data.csv", comment='#')
+df = pd.read_csv("jan-oct-2020-cycle-data.csv", comment='#')
 
-CharlivileOut = np.array(df.iloc[:,5])
-CharlivileIn = np.array(df.iloc[:,6])
+GroveRoad = np.array(df.iloc[:,5])
 
-dataLen = len(CharlivileOut)
+dataLen = len(GroveRoad)
 numDays = int(dataLen/24)
 
-CharlivileOutByDay = np.array_split(CharlivileOut, numDays)
-
+GroveRoadByDay = np.array_split(GroveRoad, numDays)
 
 peaks = []
 #------------get peak of each day-----------------
 for i in range(0, numDays):
-    peaks.append(max(CharlivileOutByDay[i]))
+    peaks.append(max(GroveRoadByDay[i]))
 
-print(numDays, peaks)
+#print(numDays, peaks)
 days = list(range(0, numDays))
 
 for i in range (0, numDays):
-    #print(i, len(x), len(CharlivileOutByDay[i]))
+    #print(i, len(x), len(GroveRoadByDay[i]))
     plt.scatter(days[i], peaks[i])
 plt.xlabel("Hour"); plt.ylabel("Cyclists")
 plt.legend(['In'])
@@ -34,9 +32,9 @@ hour = list(range(0,24))
 plt.rc('font', size=18)
 plt.rcParams['figure.constrained_layout.use'] = True
 
-for i in range (0, len(CharlivileOutByDay)):
-    #print(i, len(x), len(CharlivileOutByDay[i]))
-    plt.plot(hour, CharlivileOutByDay[i])
+for i in range (0, len(GroveRoadByDay)):
+    #print(i, len(x), len(GroveRoadByDay[i]))
+    plt.plot(hour, GroveRoadByDay[i])
 plt.xlabel("Hour"); plt.ylabel("Cyclists")
 plt.legend(['In'])
 plt.title("Logistic Regression")
